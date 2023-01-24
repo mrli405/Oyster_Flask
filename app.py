a4281b4d97451e1bc1f6574bf297f628c19ddd9a
yyj <yijiey.405@gmail.com>
2023-01-24 23:59:20 +1100

    /cmd1 url for flask run python AI code

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, redirect
+import subprocess
+import shlex
+
 
 app = Flask(__name__)
 
+process = None
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/cmd1')
+def cmd1():
+    # get json data
+    # store in local directory
+    global process
+    if process is None or process.poll() is not None:
+        process = subprocess.Popen(
+           shlex.split("python aipart/labelme2coco.py aipart/datasets_20220822 aipart/datasets_20220822_coco/c3 --labels aipart/labels.txt"))
+        return redirect('success')
+    return 'Waiting!'
+
+@app.route('/success')
+def success():
+    return 'Successful!'
 
 if __name__ == '__main__':
     app.run()
